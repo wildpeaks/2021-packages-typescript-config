ceb32062b3aeb0af394ab6b6b9eefbb5352fe86f
Cecile Muller <cecilemuller@users.noreply.github.com>
2019-11-13 13:49:27 +0100

    test: Add first fixture test
    Also moved the JSON Schema tests with the fixtures: they're very short and they run before the long tests that way.

--- test/fixtures.spec.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env node, mocha */
-/* eslint-disable no-undefined */
-'use strict';
-
-//
-// spawn + tsc is more reliable, and I can just write the tsconfig in fixtures
-//
---
+++ test/fixtures/basic/src/main.ts
@@ -0,0 +1,4 @@
+/* eslint-env node */
+
+const text: string = String(process.argv.join(','));
+console.log(text);
