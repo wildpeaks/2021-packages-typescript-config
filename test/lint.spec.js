b388bd7842bdf9800fe86164fb7652318f66dcac
Cecile Muller <cecilemuller@users.noreply.github.com>
2019-11-13 20:46:05 +0100

    test: Update lint script

--- test/lint.spec.js
+++ test/lint.spec.js
@@ -1,36 +1,58 @@
 /* eslint-env node, mocha */
 /* eslint-disable prefer-arrow-callback */
 'use strict';
-const {deepStrictEqual} = require('assert');
+const {strictEqual, deepStrictEqual} = require('assert');
 const {readFileSync} = require('fs');
 const {join} = require('path');
 
 const Ajv = require('ajv');
 const draft04 = require('ajv/lib/refs/json-schema-draft-04.json');
-const configSchema = JSON.parse(readFileSync(join(__dirname, 'config.schema.json'), 'utf8'));
+const configSchema = JSON.parse(readFileSync(join(__dirname, 'preset.schema.json'), 'utf8'));
 const tsconfigSchema = JSON.parse(readFileSync(join(__dirname, 'tsconfig.schema.json'), 'utf8'));
-const configs = require('../src/configs');
+const presets = require('../src/presets');
 
-
-function assertProperties(configId){
+function assertProperties(presetId){
 	const linter = new Ajv();
 	const validate = linter.compile(configSchema);
-	validate(configs[configId].tsconfig);
+	validate(presets[presetId]);
 	deepStrictEqual(validate.errors, null);
 }
 
-function assertSchema(configId){
+function assertSchema(presetId){
 	const linter = new Ajv({meta: false, schemaId: 'id'});
 	linter.addMetaSchema(draft04);
 	const validate = linter.compile(tsconfigSchema);
-	validate(configs[configId].tsconfig);
+	validate(presets[presetId].tsconfig);
 	deepStrictEqual(validate.errors, null);
 }
 
-for (const configId in configs){
-	describe(`Lint: ${configId}`, function(){
-		it('Properties', assertProperties.bind(null, configId));
-		it('JSON Schema', assertSchema.bind(null, configId));
-	});
+
+// Paths are unfortunately relative to the shared config package:
+// https://github.com/Microsoft/TypeScript/issues/29172
+function assertPath(text, id){
+	strictEqual(text.startsWith('../../../') || text.startsWith('@wildpeaks'), id);
 }
 
+
+function assertPaths(presetId){
+	const preset = presets[presetId];
+	for (const id of ['outDir', 'outFile', 'rootDir']){
+		if (id in preset){
+			assertPath(preset[id]);
+		}
+	}
+	for (const id of ['include', 'files']){
+		if (id in preset){
+			assertPath(preset[id]);
+		}
+	}
+}
+
+
+for (const presetId in presets){
+	describe(`Lint: ${presetId}`, function(){
+		it('Preset', assertProperties.bind(null, presetId));
+		it('TS Config', assertSchema.bind(null, presetId));
+		it('Paths', assertPaths.bind(null, presetId));
+	});
+}
