68ccb66cc757328927bf1d6a61885abecdb781ed
Cecile Muller <cecilemuller@users.noreply.github.com>
2019-11-13 01:37:06 +0100

    test: Add placeholder for tests suite

---
+++ test/fixtures.spec.js
@@ -0,0 +1,9 @@
+/* eslint-env node, mocha */
+'use strict';
+
+// Not sure I really need @types/node given I write the tests in JS directly
+// although I'm not sure if I can really use the TS compiler in JS, it might require TS to work properly
+
+//
+//
+//
