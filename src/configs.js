f3fb980a7e8dd87dfc2185701ebfd1bc8054e416
Cecile Muller <cecilemuller@users.noreply.github.com>
2019-11-13 15:34:28 +0100

    chore: Add configs
    Only "node" for now

---
+++ src/configs.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+	node: {
+		title: 'Node',
+		description: '',
+		tsconfig: {
+			compilerOptions: {
+				newLine: 'LF',
+				alwaysStrict: true,
+				noEmitOnError: true,
+				noImplicitAny: true,
+				noImplicitReturns: true,
+				noImplicitThis: true,
+				noUnusedLocals: true,
+				noUnusedParameters: true,
+				strictNullChecks: true,
+				preserveConstEnums: true,
+				useDefineForClassFields: true,
+
+				moduleResolution: 'node',
+				resolveJsonModule: true,
+				allowJs: false,
+				module: 'commonjs',
+				lib: ['es2017'],
+
+				target: 'es2017',
+				sourceMap: false,
+				removeComments: true,
+				outDir: 'lib'
+			},
+			include: [
+				'./src/*.d.ts',
+				'./src/*.ts',
+				'./src/**/*.ts',
+				'./src/node_modules/**/*.ts'
+			]
+		}
+	}
+};
