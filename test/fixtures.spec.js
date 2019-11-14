6f78cd4127f5471758c7cf84e92127a09335dfb1
Cecile Muller <cecilemuller@users.noreply.github.com>
2019-11-14 10:52:56 +0100

    test: List files for compile tests

--- test/fixtures.spec.js
+++ test/fixtures.spec.js
@@ -1,12 +1,15 @@
 /* eslint-env node, mocha */
 /* eslint-disable prefer-arrow-callback */
 'use strict';
+const {strictEqual, deepStrictEqual} = require('assert');
 const {exec} = require('child_process');
 const {writeFileSync} = require('fs');
+const {relative} = require('path');
 const {copySync, removeSync, mkdirpSync} = require('fs-extra');
 const {join} = require('path');
 const fixturesFolder = join(process.cwd(), 'test/fixtures');
 const packagesFolder = join(process.cwd(), 'packages');
+const rreaddir = require('recursive-readdir');
 
 
 function compile(fixtureFolder){
@@ -34,20 +37,26 @@ before('Setup', function(){
 });
 
 
+async function getFiles(folder){
+	const files = await rreaddir(folder);
+	return files.map(filepath => relative(folder, filepath).replace(/\\/g, '/')).sort().filter(filepath => !filepath.startsWith('node_modules'));
+}
+
+
 async function compileFixture(fixtureId, configId){
 	const folder = join(fixturesFolder, fixtureId);
 	writeFileSync(join(folder, 'tsconfig.json'), JSON.stringify({extends: `@wildpeaks/tsconfig-${configId}`}), 'utf8');
-	await compile(folder);
-	return {
-		folder,
-		files: [
-			'AAAAAAAAAAAAAAAAA',
-			'AAAAAAAAAAAAAAAAA',
-			'AAAAAAAAAAAAAAAAA',
-			'AAAAAAAAAAAAAAAAA',
-			'AAAAAAAAAAAAAAAAA'
-		]
-	};
+	const filesBefore = await getFiles(folder);
+
+	let throws = false;
+	try {
+		await compile(folder);
+	} catch(e){
+		throws = e;
+	}
+
+	const filesAfter = await getFiles(folder);
+	return {throws, folder, filesBefore, filesAfter};
 }
 
 
@@ -55,14 +64,31 @@ describe('Package: Node', function(){
 
 	it(`Basic`, /* @this */ async function(){
 		this.slow(15000);
-		const {folder, files} = await compileFixture('basic', 'node');
+		const {throws, filesBefore, filesAfter} = await compileFixture('basic', 'node');
+
+		strictEqual(throws, false, 'No error');
+
+		deepStrictEqual(
+			filesBefore,
+			[
+				'src/main.ts',
+				'tsconfig.json'
+			],
+			'Files before'
+		);
+
+		deepStrictEqual(
+			filesAfter,
+			[
+				'lib/main.js',
+				'src/main.ts',
+				'tsconfig.json'
+			],
+			'Files after'
+		);
 
 		//
-		// check list of generated files
-		//
-
-		//
-		// TODO run
+		// TODO run in Node (or Puppeteer for web config)
 		//
 	});
 });
